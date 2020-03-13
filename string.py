1d5ac89c071bb1ad73a940dca62cc2e502de93bc
C-Rachel <c_rachel@sina.cn>
2020-03-13 09:03:43 +0800

    str up

---
+++ string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+str = 'Markypooh';
+
+print(str); #输出字符串str
+print(str[0:4]); #输出第一个到第四个字符
+print(str[5:]); #输出第六个开始之后的所有字符
+print(str[0:-4]); #输出第一个到倒数第五个的所有字符
+print(str * 2); #输出字符串两次
+print(str + 'biu'); #字符串拼接
