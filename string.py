7f5ccfeb5ae93771fef818fad74572716d14faf3
C-Rachel <c_rachel@sina.cn>
2020-03-17 09:36:16 +0800

    str up

--- string.py
+++ string.py
@@ -9,16 +9,24 @@ print(str[0:-4]); #输出第一个到倒数第五个的所有字符
 print(str * 2); #输出字符串两次
 print(str + 'biu'); #字符串拼接
 
-num = int(input("请输入数字:"))
-
-if (num == 10):
-    print ("恭喜你，数字正确！")
-else:
-    print ("数字不正确！")
-
 # 斐波纳契数列	
 a, b = 0, 1
 while b < 30:
     print(b)
     a, b = b, a+b
 	
+print("Mark \nBambam")
+
+# """..."""使用三重引号，回车换行会包含在字符串中，如果不想包含，在行尾加上\
+# 例子如下：
+print("""\
+	Jackson
+JYP
+""")
+	
+num = int(input("请输入数字:"))
+
+if (num == 10):
+    print ("恭喜你，数字正确！")
+else:
+    print ("数字不正确！")
